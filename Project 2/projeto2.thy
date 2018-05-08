03304a56ec39ca6926503346cf5e48a6703f9984
Vinicius Barbosa de Medeiros <bmvinicius11@gmail.com>
2018-05-08 19:26:03 -0300

    Provas no mesmo arquivo

--- Project 2/projeto2.thy
+++ Project 2/projeto2.thy
@@ -26,4 +26,31 @@ theorem prova1:
     qed
   from p_qr and p_pq_and_pr and qr_pq_and_pr show "(P\<or>Q)\<and>(P\<or>R)" by (rule disjE)
 qed
+
+theorem prova2:
+  assumes pq: "P\<or>Q"
+  shows "\<not>(\<not>P \<and> \<not>Q)"
+
+proof -
+  have npnqFalse: "(\<not>P \<and> \<not>Q) \<Longrightarrow> False"
+  proof -
+    assume npnq: "(\<not>P \<and> \<not>Q)"
+    from npnq have np: "\<not>P" by (rule conjE)
+    from npnq have nq: "\<not>Q" by (rule conjE)
+    have pfalse: "P \<Longrightarrow> False"
+    
+    proof -
+      assume p: "P"
+      from np and p show "False" by (rule notE)
+    qed
+
+    have qfalse: "Q \<Longrightarrow> False"
+    proof -
+      assume q: "Q"
+      from nq and q show "False" by (rule notE)
+    qed
+    from pq and pfalse and qfalse show contra:"False" by (rule disjE)
+  qed
+  from npnqFalse show "\<not>(\<not>P\<and>\<not>Q)" by (rule notI)
+qed
 end
